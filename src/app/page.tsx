b5809d2f0b02e78082b3bd31061f745e101f2037
Spencer Faith <13sfaith@gmail.com>
2025-04-29 20:07:01 -0700

    basic graph builder with dagre

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,45 @@
 import styles from "./page.module.css";
 import { ReactFlow, type Node, type Edge } from '@xyflow/react';
+import { buildGraph } from './graphBuilder';
+
 
 import '@xyflow/react/dist/style.css';
 
 const initalNodes: Node[] = [
-  { id: '1', position: { x: 0, y: 0}, data: { label: '1'} },
-  { id: '2', position: { x: 0, y: 100}, data: { label: '2'} },
 ]
-const initialEdges: Edge[] = [{ id: 'e1-2', source: '1', target: '2' }]
+// const initialEdges: Edge[] = [{ id: 'e1-2', source: '1', target: '2' }]
+const initialEdges: Edge[] = []
+
+async function populateNodes() {
+  let graph = await buildGraph()
+
+  graph.nodes().forEach((id) => {
+    let nodePosition = graph.node(id)
+    let node: Node = {
+      id: id,
+      position: { x: nodePosition.x, y: nodePosition.y },
+      data: { label: nodePosition.label }
+    };
+    initalNodes.push(node)
+  })
+
+  let edgeNumber = 0
+  graph.edges().forEach((id) => {
+    let edgePosition = graph.edge(id)
+    console.log(id)
+    let edge: Edge = {
+      id: 'e-' + edgeNumber,
+      source: id.v,
+      target: id.w,
+    }
+    edgeNumber++;
+    initialEdges.push(edge);
+  })
+
+}
 
 export default function Home() {
+  populateNodes()
   return (
     <div style={{ width: '100vw', height: '110vh' }}>
       <ReactFlow colorMode="dark" nodes={initalNodes} edges={initialEdges} />
